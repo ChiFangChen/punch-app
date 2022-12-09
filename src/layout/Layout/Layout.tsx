c6afab7ba83e4d095f36f60d816dcc79a33fe31a
Kiki Chen <kiki830514@gmail.com>
2022-12-09 22:04:27 +0800

    feat: get init data

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,15 +1,25 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useAppDispatch, useAppSelector, actions } from '@/model';
 import Header from '../Header';
 import { StyledLayout, StyledContent } from './styles';
 
-const Layout = () => (
-  <StyledLayout>
-    <Header></Header>
+const Layout = () => {
+  const dispatch = useAppDispatch();
+  const isReady = useAppSelector((state) => state.config.isReady);
 
-    <StyledContent>
-      <Outlet />
-    </StyledContent>
-  </StyledLayout>
-);
+  useEffect(() => {
+    dispatch(actions.getConfigAsync());
+    dispatch(actions.getHistoryAsync());
+  }, [dispatch]);
+
+  return (
+    <StyledLayout>
+      <Header></Header>
+
+      <StyledContent>{isReady ? <Outlet /> : 'loading...'}</StyledContent>
+    </StyledLayout>
+  );
+};
 
 export default Layout;
