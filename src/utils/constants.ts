f60d10e4012a75d5f7ec53a1ed8ab12d2b0421eb
Kiki Chen <kiki830514@gmail.com>
2022-12-11 21:41:23 +0800

    chore: update the default coordinate

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,5 +9,5 @@ export const COORDINATE = 'COORDINATE';
 export const MIN_RANGE = 5;
 export const MAX_RANGE = 20;
 
-export const DEFAULT_LATITUDE = 23;
-export const DEFAULT_LONGITUDE = 123;
+export const DEFAULT_LATITUDE = 37.5021832;
+export const DEFAULT_LONGITUDE = 127.0240719;
