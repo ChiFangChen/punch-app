575c97061b1c110713ba78af698195d5078f33fd
Kiki Chen <kiki830514@gmail.com>
2022-12-11 16:10:40 +0800

    feat: update styles

--- src/pages/Home/ClockBlock/styles.ts
+++ src/pages/Home/ClockBlock/styles.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 const StyledClockBlock = styled('div')`
   display: flex;
   flex-direction: column;
-  margin: 0 30px;
+  margin: 0 50px;
 `;
 
 const StyledClock = styled('div')`
--- src/pages/Home/History/styles.ts
+++ src/pages/Home/History/styles.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 const StyledHistory = styled('div')`
-  flex: 1;
+  flex: 1.5;
   align-self: flex-start;
 `;
 
