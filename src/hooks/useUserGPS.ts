35939ad4e8e9895bb23b9f108412e459d40f4ffc
Kiki Chen <kiki830514@gmail.com>
2022-12-19 16:47:38 +0800

    fix: update mark to be all in English

--- src/hooks/useUserGPS.ts
+++ src/hooks/useUserGPS.ts
@@ -34,9 +34,9 @@ const useUserGPS = () => {
             const errorHandler = (err: any) => {
               dispatch(actions.updateUser({ gps: false }));
               console.log(err);
-              // 1	PERMISSION_DENIED	沒有獲取裝置定位的權限
-              // 2	POSITION_UNAVAILABLE	位置資訊獲取錯誤
-              // 3	TIMEOUT	在 Timeout 前未取得定位資訊
+              // 1	PERMISSION_DENIED: no GPS permission
+              // 2	POSITION_UNAVAILABLE:	error when getting position
+              // 3	TIMEOUT: do not get the position before timeout
             };
 
             navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
