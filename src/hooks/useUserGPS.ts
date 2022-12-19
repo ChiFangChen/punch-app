cbd2375cf5a36706652edb64dffd32f23bb1cfd0
Kiki Chen <kiki830514@gmail.com>
2022-12-19 17:15:36 +0800

    feat: detect the location  permission changing

--- src/hooks/useUserGPS.ts
+++ src/hooks/useUserGPS.ts
@@ -7,43 +7,56 @@ const useUserGPS = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    let geoId: number;
+    let geoId: number | null = null;
+
+    const detectPermission = (permissionStatusState: string) => {
+      if (permissionStatusState === 'denied') {
+        // navigator.permissions.revoke({ name: 'geolocation' }) is no longer supported
+        dispatch(actions.updateUser({ gps: false }));
+        alert(t('turn-on-permission'));
+      } else {
+        dispatch(actions.updateUser({ gps: true }));
+        if ('geolocation' in navigator) {
+          const successHandler = ({
+            coords: { latitude, longitude },
+          }: {
+            coords: { latitude: number; longitude: number };
+          }) => {
+            dispatch(
+              actions.updateUser({
+                coordinates: [latitude, longitude],
+                gps: true,
+              })
+            );
+          };
+
+          const errorHandler = (err: any) => {
+            dispatch(actions.updateUser({ gps: false }));
+            console.log(err);
+            // 1	PERMISSION_DENIED: no GPS permission
+            // 2	POSITION_UNAVAILABLE:	error when getting position
+            // 3	TIMEOUT: do not get the position before timeout
+          };
+
+          geoId = navigator.geolocation.watchPosition(successHandler, errorHandler);
+        } else {
+          alert(t('browser-not-support'));
+        }
+      }
+    };
 
     if ('permissions' in navigator) {
       navigator.permissions.query({ name: 'geolocation' }).then((permissionStatus) => {
-        if (permissionStatus.state === 'denied') {
-          // navigator.permissions.revoke({ name: 'geolocation' }) is no longer supported
-          dispatch(actions.updateUser({ gps: false }));
-          alert(t('turn-on-permission'));
-        } else {
-          dispatch(actions.updateUser({ gps: true }));
-          if ('geolocation' in navigator) {
-            const successHandler = ({
-              coords: { latitude, longitude },
-            }: {
-              coords: { latitude: number; longitude: number };
-            }) => {
-              dispatch(
-                actions.updateUser({
-                  coordinates: [latitude, longitude],
-                  gps: true,
-                })
-              );
-            };
-
-            const errorHandler = (err: any) => {
-              dispatch(actions.updateUser({ gps: false }));
-              console.log(err);
-              // 1	PERMISSION_DENIED: no GPS permission
-              // 2	POSITION_UNAVAILABLE:	error when getting position
-              // 3	TIMEOUT: do not get the position before timeout
-            };
-
-            geoId = navigator.geolocation.watchPosition(successHandler, errorHandler);
-          } else {
-            alert(t('browser-not-support'));
+        /* eslint no-param-reassign: ["error", { "props": false }] */
+        permissionStatus.onchange = () => {
+          if (geoId) {
+            navigator.geolocation.clearWatch(geoId);
+            geoId = null;
           }
-        }
+          detectPermission(permissionStatus.state);
+        };
+
+        detectPermission(permissionStatus.state);
       });
     }
 
